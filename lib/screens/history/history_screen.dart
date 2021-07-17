7bed9c80ae544618168566c5c446a3e36510e99b
Andrew Moreno <andrewmoreno1431@gmail.com>
2021-07-17 10:35:52 -0700

    Added Sliver functionality

--- lib/screens/history/history_screen.dart
+++ lib/screens/history/history_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../constraints.dart';
 import '../../models/release_data.dart';
-import 'local widgets/history_header.dart';
 import 'local widgets/history_tile.dart';
 
 class History extends StatelessWidget {
@@ -13,26 +13,33 @@ class History extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const HistoryHeader(),
-            Expanded(
-              child: ListView.builder(
-                itemCount: ReleaseData().releases.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 20, top: 20),
-                    child: HistoryTile(
-                      title: ReleaseData().releases[index].title,
-                      artist: ReleaseData().releases[index].artist,
-                      albumArt: ReleaseData().releases[index].albumArt,
-                      releaseDate: ReleaseData().releases[index].releaseDate,
-                    ),
-                  );
-                },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: const Color(0xFF2E2E3E),
+              floating: true,
+              title: Text(
+                "History",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4!
+                    .copyWith(color: kAccentRed),
               ),
+              elevation: 100,
             ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate((_, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: HistoryTile(
+                    title: ReleaseData().releases[index].title,
+                    artist: ReleaseData().releases[index].artist,
+                    albumArt: ReleaseData().releases[index].albumArt,
+                    releaseDate: ReleaseData().releases[index].releaseDate,
+                  ),
+                );
+              }, childCount: ReleaseData().releases.length),
+            )
           ],
         ),
       ),
